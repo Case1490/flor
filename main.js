10e63bc3d54bab66daec7d4d6fe196ec35c2e211
Renzo Ramos <108292525+Case1490@users.noreply.github.com>
2023-09-21 00:19:27 -0500

    commit

---
+++ main.js
@@ -0,0 +1,17 @@
+
+
+function iniciar() {
+    var elementoCentro = document.querySelector(".contenedor .centro");
+    elementoCentro.addEventListener("mousemove", girarGirasol);
+}
+
+const elementoPetalo1 = document.querySelector(".contenedor .petalo");
+const elementoPetalo2 = document.querySelector(".contenedor .petalo2");
+const elementoPetalo3 = document.querySelector(".contenedor .petalo3");
+const elementoPetalo4 = document.querySelector(".contenedor .petalo4");
+const elementoPetalo5 = document.querySelector(".contenedor .petalo5");
+const elementoPetalo6 = document.querySelector(".contenedor .petalo6");
+const elementoPetalo7 = document.querySelector(".contenedor .petalo7");
+const elementoPetalo8 = document.querySelector(".contenedor .petalo8");
+
+var duracionAnimacion = 500;
